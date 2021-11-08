362b9701177a7d85e065ba2364d314b8b6bb9884
Corinna-Genchi <corinnagenchi@gmail.com>
2021-11-08 22:00:35 +0100

    tested admin endpoints, added form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 
@@ -11,19 +11,41 @@ const[activities, setActivities] = useState("");
 
   return (
     <div className="App">
+      <div className="container">
      <h1>Playdate</h1>
-     <h2>Activities</h2>
-     
-     <div id="search_activity">
-     <form>
-      <label>Search for an activity by name</label>
-     <input/>
-     <div id="filters">
-       <input/>
-       <input/>
+     <div id="activities_catalogue">
+       <h2>Activities</h2>
      </div>
-     <button>Search</button>
+     
+     {/* {FORM} */}
+     <div id="search_activity" className="container">
+     <form>
+      <label>Search for keyword</label>
+     <input className="form-control" type="text" placeholder="ball, chalks, montessori"/>
+     <div id="filters">
+       <div className="form-check">
+  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+  <label className="form-check-label" htmlFor="flexRadioDefault1">
+    Outdoor
+  </label>
+</div>
+<div class="form-check">
+  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+  <label className="form-check-label" htmlFor="flexRadioDefault2">
+    Indoor
+  </label>
+</div>
+<label id="age_slider">Age</label>
+<div class="slidecontainer"aria-labelledby="age_slider">
+  <label aria-label="minimum_age">1</label>
+  <input type="range" min="1" max="10"  className="slider" id="myRange" />
+  <label aria-label="maximum_age">10</label>
+</div>
+
+     </div>
+     <button aria-label="Search_button" type="button" className="btn btn-outline-warning">Search</button>
      </form>
+     </div>
      </div>
     </div>
   );
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require("../model/helper");
 
-/* GET home page. */
+/* GET home page. */ //it works
 router.get('/', async function(req, res, next) {
   try{
     const results = await db("SELECT * FROM activities ORDER BY id ASC;");
@@ -13,23 +13,37 @@ router.get('/', async function(req, res, next) {
   }
 });
 
-//POST new activity
+//POST new activity - it works in local host, I see error in postman
 router.post('/', async function(req, res, next) {
  
   const {name, age_range, indoor, outdoor, description} = req.body;
 
   try{
-    await db(`INSERT INTO activities (name, age_range, indoor, outdoor, description) VALUES (${name}, ${age_range}, ${indoor}, ${outdoor}, ${description})`)
+    await db(`INSERT INTO activities (name, age_range, indoor, outdoor, description) VALUES ('${name}', '${age_range}', '${indoor}', '${outdoor}', '${description}');`)
     const results = await db("SELECT * FROM activities ORDER BY id ASC;");
-    res.send(results.data);
+    res.status(201).send(results.data);
   }
   catch(err){
        res.status(500).send(err);
   }
 });
 
+//UPDATE an acitivity //not working
+// router.put('/:id', async function(req, res, next) {
+//   const { id } = req.params;
+//   const {age_range, indoor, outdoor, description} = req.body;
 
-//DELETE an activity by ID
+//   try{
+//     await db(`UPDATE activities  SET age_range = '${age_range}', indoor='${indoor}', outdoor='${outdoor}', description='${description}' WHERE id="${id}";`)
+//     const results = await db(`SELECT FROM activities WHERE id = "${id}";`);
+//     res.status(204).send(results.data);
+//   }
+//   catch(err){
+//        res.status(500).send(err);
+//   }
+// });
+
+//DELETE an activity by ID -it works in local but I see error in postman
 
 router.delete('/:id', async function(req, res, next) {
   
